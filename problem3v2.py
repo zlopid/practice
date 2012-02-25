e9c429234f9f5a5708e58702296dac5b19cc0a57
Aaron Fetterman <zlopid@gmail.com>
2012-02-25 14:15:15 -0500

    After reading the forum for problem 3, I realized I was missing some key facts about primes, so I've re-implemented it in a much simpler way after figuring out my missing knowledge

---
+++ problem3v2.py
@@ -0,0 +1,26 @@
+import math
+
+def smallestFactor(num):
+	'''Find the smallest factor of a number, which is always a prime'''
+	# We know the smallest factor is prime because if it was not,
+	# it would be the product of two smaller numbers, and those numbers
+	# would be factors of the given number.
+	f = 2
+	while (num % f != 0):
+		f += 1
+	return f
+
+def largestPrimeFactor(num):
+	'''Find the largest factor of a number by recursively removing all smaller factors'''
+	sf = smallestFactor(num)
+	
+	# Base case: num is prime, and has no smaller factors 
+	if (sf == num):
+		return num
+		
+	# Induction case: num is composite, = sf * c
+	# and the largest factor of num is the largest factor of c
+	return largestPrimeFactor(num/sf)
+
+if __name__ == "__main__":
+	print largestPrimeFactor(600851475143)
