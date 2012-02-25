67c2bcc0825f874f0f282a60eb58e38b4b28aa22
Aaron Fetterman <zlopid@gmail.com>
2012-02-25 13:04:32 -0500

    120101st to solve problem 3!
    This one was trickier, because a simple brute-force generation of primes would take too long, so I had to start with the list of factors.

---
+++ problem3.py
@@ -0,0 +1,56 @@
+import math
+
+def primes():
+	'''Generate endless new primes. Warning: this will be slow for larger numbers'''
+	primes = []
+	n = 2
+	while True:
+		for p in primes:
+			if (n % p == 0):
+				break
+		else:
+			yield n
+			primes.append(n)
+		n += 1
+
+def factors(num):
+	'''Generates all factors of a given number in increasing order'''
+	
+	# for every factor n, there is an opposite factor num/n
+	# store them as we go so we can yield factors in order
+	oppositeFactors = []
+	
+	# because this tracks opposite factors, it only needs to
+	# iterate until the mid-point, sqrt(num)
+	for n in range(2, int(math.sqrt(num))):
+		if (num % n == 0):
+			yield n
+			oppositeFactors.append(num/n)
+			
+	while len(oppositeFactors) > 0:
+		yield oppositeFactors.pop()
+
+
+def primeFactors(num):
+	'''Returns a list of the prime factors of the given num'''
+	remainingFactors = [f for f in factors(num)]
+	getPrimes = primes()
+	p = getPrimes.next()
+	
+	# Eliminate factors that are divisible by primes until all factors are known to be prime
+	
+	# When p > sqrt(max(remainingFactors)), then
+	#   all remaining factors are not divisible by any primes less than p
+	# 	if any factor was divisible by a prime greater than or equal to p,
+	#		then the factor would also be divisible by factor/p
+	#		and factor/p must be < sqrt(factor)
+	#		but that's impossible because we've already tested all primes < sqrt(factor)
+	#	therefore, no factor is divisible by a prime greater than or equal to p
+	#	therefore, all remaining factors are prime
+	while len(remainingFactors) > 0 and p < math.sqrt(max(remainingFactors)):
+		remainingFactors= filter(lambda f: f % p != 0 or f == p, remainingFactors)
+		p = getPrimes.next()
+	return remainingFactors
+	
+if __name__ == "__main__":
+	print max(primeFactors(600851475143))
